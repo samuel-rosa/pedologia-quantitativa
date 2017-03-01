73875c84a0f0dd8baa126bc5f3a144b3f706d0ed
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-03-01 15:35:14 -0300

    criado arquivo para código R com funções de ajuda;

---
+++ helpme.R
@@ -0,0 +1,7 @@
+helpme <- 
+  paste(
+    "> Este capítulo está em desenvolvimento. Visite a",
+    "[página do projeto](https://github.com/samuel-rosa/pedologia-quantitativa/issues) se tiver",
+    "alguma sugestão, dúvida ou estiver disposto a colaborar. Sua opinião/ajuda é muito importante!",
+    "\n", "Alessandro Samuel-Rosa"
+  )
