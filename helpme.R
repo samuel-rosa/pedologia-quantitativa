65f2afe3dac042544561f932fce44e79bab454a1
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-03-02 08:36:52 -0300

    detalhes adicionados aos modelos determinísticos

--- helpme.R
+++ helpme.R
@@ -2,6 +2,5 @@ helpme <-
   paste(
     "> Este capítulo está em desenvolvimento. Visite a",
     "[página do projeto](https://github.com/samuel-rosa/pedologia-quantitativa/issues) se tiver",
-    "alguma sugestão, dúvida ou estiver disposto a colaborar. Sua opinião/ajuda é muito importante!",
-    "\n", "Alessandro Samuel-Rosa"
+    "alguma sugestão, dúvida ou estiver disposto a colaborar. Sua opinião/ajuda é muito importante!"
   )
